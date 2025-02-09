8a9a4369551f20401df169e5657197d62ecc4c0d
SK001\tk-sa <tk-satoken1124@outlook.jp>
2025-02-09 11:25:07 +0900

    エンコード方式変更

--- main/java/com/example/demo/utility/Common.java
+++ main/java/com/example/demo/utility/Common.java
@@ -1,9 +1,9 @@
 package com.example.demo.utility;
 
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
 
 import com.example.demo.log.LogCreator;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -35,17 +35,16 @@ public final class Common {
 	}
 	
 	/**
-	* base64文字列=>JsonNode
+	* form-url-encoded文字列=>JsonNode
 	* @param base64文字列
 	* @return JsonNode
 	*/
 	public static JsonNode base64ToJsonNode(String item) {
-		var byteContents = Base64.getDecoder().decode(item);
-		var stringSerchParams = new String(byteContents,StandardCharsets.UTF_8);
+		var stringJson = URLDecoder.decode(item,StandardCharsets.UTF_8);
 		
 		var objectMapper = new ObjectMapper();
 		try {
-			var jsonNodes = objectMapper.readTree(stringSerchParams);
+			var jsonNodes = objectMapper.readTree(stringJson);
 			return jsonNodes;
 		} catch (Exception e) {
 			LogCreator.error(e.getMessage());
